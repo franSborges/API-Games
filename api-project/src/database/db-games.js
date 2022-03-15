7e6052b0cba9c7df2d8b2ae82d4069c8fc0a04c1
franSborges <borgesfran988@gmail.com>
2022-03-14 23:07:52 -0300

    third commit

--- api-project/src/database/db-games.js
+++ api-project/src/database/db-games.js
@@ -1,35 +1,6 @@
-// const DATABASE = {
-//   games: [
-//     {
-//       id: 1,
-//       name: 'Call of Duty',
-//       year_release: 2003,
-//       price: 80
-//     },
-//     {
-//       id: 2,
-//       name: 'Escape From Tarkov',
-//       year_release: 2016,
-//       price: 100
-//     },
-//     {
-//       id: 3,
-//       name: 'Minecraft',
-//       year_release: 2011,
-//       price: 40
-//     },
-//     {
-//       id: 4,
-//       name: 'It Takes Two',
-//       year_release: 2021,
-//       price: 90
-//     }
-//   ]
-// }
-
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('apigames', 'root', '306433', {
+const connection = new Sequelize('confidential', 'confidential', 'confidential', {
   host: 'localhost',
   dialect: 'mysql'
 })
